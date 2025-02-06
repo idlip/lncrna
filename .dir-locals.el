dcc0f96a845aa4a32c4cb0aeca010cbe2e29fce8
Dilip <igoldlip@gmail.com>
2025-02-06 15:27:08 +0530

    directory variable and setup

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+;; .dir-locals.el
+((nil . ((eval . (progn (highlight-regexp "TODO:" 'org-todo)  (highlight-regexp "NOTE:" 'flymake-note-echo) (highlight-regexp "FIXME:" 'org-priority))))
+               ))
